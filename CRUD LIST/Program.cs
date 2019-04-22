7a7df5a1a91915ea6dd973a3ab743864110f3f1e
Pavel <pavelchi91@gmail.com>
2019-04-23 01:34:46 +0300

    Version 1.0.0 Functional, tested and has a HELP MENU

--- CRUD LIST/Program.cs
+++ CRUD LIST/Program.cs
@@ -10,7 +10,7 @@ namespace CRUD_LIST
         public string TimeStamp;
         
         public Item(string _name, string _position, string _timestamp) 
-        {//constructor that takes name input and a timestamp as parametrs
+        {//constructor that takes name, position and a timestamp as parametrs
             this.Name = _name;
             this.Position = _position; 
             this.TimeStamp = _timestamp;
@@ -40,89 +40,128 @@ namespace CRUD_LIST
             List<Item> ItemList = new List<Item>(); // a list that holds item objects
             bool exit = false;
             bool found = false;
-            int k = 0;
+            bool duplicate = false;
             string choice = " ";
             string search = " ";
+            string Name = " ";
+            string Position;
+            int test1, test2;
+
+            Program.Help();
 
             do
             {
-                found = false; //reseting
+                
+               
+                found = false; //reseting for a fresh loop
+                duplicate = false; //reseting for a fresh loop
                 Console.WriteLine("Item List:");
                 for (int i = 0; i < ItemList.Count; i++) ItemList[i].PrintName();
 
-                Console.WriteLine("\nWhat to do?");
+                Console.WriteLine("\nCommand list: CREATE, READ, UPDATE, DELETE, HELP, EXIT\nPlease type in a command");
                 choice = Console.ReadLine().ToUpper();
 
-                //creates new item in the list
+
+                // Create new item in the list
                 if (choice == "CREATE")
                 {
                     //creating new item in the list
-                    //need to test for name/position duplicates
-                    Console.WriteLine("Please enter item name and item position");
-                    ItemList.Add(new Item(Console.ReadLine(), Console.ReadLine(), (DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss")));
-
+                    //need to test for name/position duplicates, correctness of input
+                    Console.WriteLine("Please enter new item's Name and Position.");
+                    string[] tokens = Console.ReadLine().Split();
+                    if (tokens.Length == 2) // check that there were 2 inputs
+                    {
+                        Name = tokens[0];
+                        Position = tokens[1];
+                        for (int i = 0; i < ItemList.Count; i++) //checking for duplicates
+                        {
+                            if (ItemList[i].Name == Name || ItemList[i].Position == Position)
+                            {
+                                Console.WriteLine("Item attributes have to be unique. Type in HELP for HELP MENU");
+                                duplicate = true;
+                                break;
+                            }
+                        }
+                        if (!duplicate)//There must be no duplicates in the list (identical names and positions)
+                        {
+                            if (int.TryParse(Position, out test1) && !(int.TryParse(Name, out test2))) //Position must be a number, Name must not be a number
+                            {
+                                ItemList.Add(new Item(Name, Position, (DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss"))); //constructor takes name, position and time stamp
+                                Console.Clear();
+                            }
+                            else Console.WriteLine("Incorrect input. Name must be a string, position must be an integer. Type in HELP for HELP MENU.");
+                        }
+                    }
+                    else Console.WriteLine("Incorrect input. Input format: Name Position (Example: Apple 1). Type in HELP for HELP MENU");
                 }
 
                 // Read an object, find it by it's name or position
                 else if (choice == "READ")
                 {
-
-                    
-                    Console.WriteLine("Enter the name or position of the object you want to read");
+                    Console.WriteLine("Enter the Name or Position of the item you want to read");
                     search = Console.ReadLine();
 
                     for (int i = 0; i < ItemList.Count; i++)
                     {
-                        if (ItemList[i].Name.Equals(search))
+                        if (ItemList[i].Name == search || ItemList[i].Position == search)
                         {
-                            ItemList[i].PrintInfo();
-                            found = true;
-                            break;
-                        }
-                        else if (ItemList[i].Position.Equals(search))
-                        {
+                            Console.Clear();
                             ItemList[i].PrintInfo();
                             found = true;
                             break;
                         }
                     }
-                    if (found == false) Console.WriteLine("No item found based on the search parameter"); 
+                    if (found == false) Console.WriteLine("No item found based on the search parameter");
                 }
 
-                //Update an object
+                //Update an object, works very similar to create
                 else if (choice == "UPDATE")
                 {
-                    //will need to check for duplicates
-                    Console.WriteLine("Enter the name or position of the object you want to update");
+                    Console.WriteLine("Enter the Name or Position of the item you want to update");
                     search = Console.ReadLine();
 
+                    //need to test for name/position duplicates, correctness of inputs
                     for (int i = 0; i < ItemList.Count; i++)
                     {
-                        if (ItemList[i].Name.Equals(search))
+                        if (ItemList[i].Name == search || ItemList[i].Position == search)
                         {
                             ItemList[i].PrintInfo();
                             found = true;
-                            Console.WriteLine("Type in object's new name");
-                            ItemList[i].Name = Console.ReadLine();
-                            Console.WriteLine("Type in object's new position");
-                            ItemList[i].Position = Console.ReadLine();
-                            ItemList[i].TimeStamp = (DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss");
-                            Console.WriteLine("Object updated");
-                            ItemList[i].PrintInfo();
-                            break;
-                        }
-                        else if (ItemList[i].Position.Equals(search))
-                        {
-                            ItemList[i].PrintInfo();
-                            found = true;
-                            Console.WriteLine("Type in object's new name");
-                            ItemList[i].Name = Console.ReadLine();
-                            Console.WriteLine("Type in object's new position");
-                            ItemList[i].Position = Console.ReadLine();
-                            ItemList[i].TimeStamp = (DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss");
-                            Console.WriteLine("Object updated");
-                            ItemList[i].PrintInfo();
-                            break;
+
+                            Console.WriteLine("Please enter item's new Name and new Position");
+                            string[] tokens = Console.ReadLine().Split();
+                            if (tokens.Length == 2) // check that there were 2 inputs
+                            {
+                                Name = tokens[0];
+                                Position = tokens[1];
+                                for (int j = 0; j < ItemList.Count; j++) //checking for duplicates
+                                {
+                                    if (ItemList[j].Name == Name || ItemList[j].Position == Position)
+                                    {
+                                        Console.WriteLine("Item's attributes have to be unique. Type in HELP for HELP MENU");
+                                        duplicate = true;
+                                        break;
+                                    }
+                                }
+                                if (!duplicate)//There must be no duplicates in the list (identical names and positions)
+                                {
+                                    if (int.TryParse(Position, out test1) && !(int.TryParse(Name, out test2))) //Position must be a number, Name must not be a number
+                                    {
+                                        ItemList[i].Name = Name;
+                                        ItemList[i].Position = Position;
+                                        ItemList[i].TimeStamp = (DateTime.Now).ToString("yyyy.MM.dd HH:mm:ss");
+                                        Console.Clear();
+                                        Console.WriteLine("Item updated");
+                                        ItemList[i].PrintInfo();
+                                        break;
+                                    }
+                                    else Console.WriteLine("Incorrect input. Name must be a string, Position must be an integer. Type in HELP for HELP MENU.");
+                                }
+                            }
+                            else Console.WriteLine("Incorrect input. Input format: Name Position (Example: Apple 1). Type in HELP for HELP MENU");
+
+
+
                         }
                     }
                     if (found == false) Console.WriteLine("No item found based on the search parameter");
@@ -131,22 +170,14 @@ namespace CRUD_LIST
                 //Delete an object
                 else if (choice == "DELETE")
                 {
-                    Console.WriteLine("Enter the name or position of the object you want to delete");
+                    Console.WriteLine("Enter the Name or Position of the item you want to delete");
                     search = Console.ReadLine();
 
                     for (int i = 0; i < ItemList.Count; i++)
                     {
-                        if (ItemList[i].Name.Equals(search))
+                        if (ItemList[i].Name == search || ItemList[i].Position == search)
                         {
-                            Console.WriteLine("Deleting");
-                            ItemList[i].PrintInfo();
-                            ItemList.Remove(ItemList[i]);
-                            found = true;
-                            break;
-                        }
-                        else if (ItemList[i].Position.Equals(search))
-                        {
-                            Console.WriteLine("Deleting");
+                            Console.WriteLine("Deleting:");
                             ItemList[i].PrintInfo();
                             ItemList.Remove(ItemList[i]);
                             found = true;
@@ -155,26 +186,54 @@ namespace CRUD_LIST
                     }
                     if (found == false) Console.WriteLine("No item found based on the search parameter");
                 }
+
+                //HELP
                 else if (choice == "HELP")
                 {
-                    Console.WriteLine("Placeholder for help menu");
+                    Program.Help();
                 }
+
+                //EXIT
                 else if (choice == "EXIT")
                 {
                     Console.WriteLine("Exiting the application");
                     exit = true;
                 }
+                else Console.WriteLine("Incorrect input. Type in HELP for HELP MENU");
 
             } while (exit == false);
         }
+
+
+        public static void Help()
+        {
+            Console.Clear();
+            Console.WriteLine("HELP MENU\nCommand list: CREATE, READ, UPDATE, DELETE, HELP, EXIT\nCommands are not case sensitive.\n");
+            Console.WriteLine("CREATE command creates a new item (object) in the list. Parameters: Name(string) Position(integer) separated by a space.");
+            Console.WriteLine("Time stamp is generated automatically. Both Name and Position must be unique in the list. Example CREATE\nApple 1 \n");
+
+            Console.WriteLine("READ command shows item's name, position and timestamp in the console. Parameters: Name(string) or Position(integer).");
+            Console.WriteLine("Example READ\nApple\n");
+
+            Console.WriteLine("UPDATE command updates chosen item's name, position and automatically replaces timestamp with current time.");
+            Console.WriteLine("Parameters: Name(string) or Position(integer) to select an object. Name(string) Position(integer) separated by a space to update");
+            Console.WriteLine("Example UPDATE\nApple\nOrange 2\n");
+
+            Console.WriteLine("DELETE command deletes an item from the list. Parameters: Name(string) or Position(integer).");
+            Console.WriteLine("Example DELETE\n2\n");
+
+            Console.WriteLine("HELP command opens the HELP MENU\n");
+            Console.WriteLine("EXIT command exits the application\n");
+
+            Console.WriteLine("Press any key to go to the list");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        
     }
+
 }
 
-/*
-•	Create<name> <position> - Creates an item with current timestamp and adds it to the buy list the position number.
-•	Read<name/position> - Items name, position and timestamp are shown in the console.
-•	Update<name/position> <changed name> <position> - Updates chosen items name, position and replaces timestamp with current time.
-•	Delete<name/position> - Deletes an Item
-•	Help – Shows all possible commands and descriptions
-*/
+
 
